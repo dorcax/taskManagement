4ac1ed7b60d29905444dfe2253af53d14092d645
dorcax <dorcasibrahim667@gmail.com>
2024-03-28 05:06:15 +0100

    fixed

--- server/app.js
+++ server/app.js
@@ -21,7 +21,9 @@ app.use(express.json())
 const corsOptions ={
   origin: ["http://localhost:5173", "https://task-managementweb.netlify.app"],
   credentials: true, // Access-Control-Allow-Credentials header
-  optionsSuccessStatus: 200, // HTTP
+  optionsSuccessStatus: 200, // HTTP status code for successful OPTIONS requests
+  methods: "GET, POST, PUT, DELETE", // Allowed HTTP methods
+  allowedHeaders: "Content-Type, Authorization", // Allowed headers
   }
   app.use(cors(corsOptions));
   // Middleware to allow CORS
