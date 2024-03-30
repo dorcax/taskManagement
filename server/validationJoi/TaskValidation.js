e2c206e05db30e3dff897924759e2a537607a5b2
dorcax <dorcasibrahim667@gmail.com>
2024-03-30 03:59:59 +0100

    fixed

--- server/validationJoi/TaskValidation.js
+++ server/validationJoi/TaskValidation.js
@@ -13,5 +13,17 @@ const validateTask =(task)=>{
     })
     return schema.validate(task)
 }
+const validateEditTask =(task)=>{
+    const schema =  Joi.object({
+        title:Joi.string().required(),
+        description :Joi.string().required(),
+        status:Joi.string().valid("COMPLETE","TODO","IMPORTANT").required(),
+        image:Joi.object({
+        mimetype:Joi.string().valid("image/jpeg","image/png","image/jpg").required(),
+        size:Joi.number().max(10*1024*1024).required()
+        })
+    })
+    return schema.validate(task)
+}
 
-module.exports=validateTask
+module.exports={validateTask,validateEditTask}
